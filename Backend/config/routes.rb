1958852d53cafd9a1ec5c5d262fb4595a4b66ed3
Mario <brizic.mario@gmail.com>
2014-11-22 15:32:12 +0100

    Sredjeni neki modeli i neke rute

--- Backend/config/routes.rb
+++ Backend/config/routes.rb
@@ -5,7 +5,21 @@ Rails.application.routes.draw do
   resources :pages
 
 
+
+  as :institution do
+    get "login", to: "devise/sessions#new"
+    get "logout", to: "devise/sessions#destroy"
+    get 'registracija', to: 'devise/registrations#new'
+  end
+
+  
+
+
+
+
+
   root 'pages#index'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
